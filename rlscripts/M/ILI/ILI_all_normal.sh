1e17592be0cc426f7f9f514c97d1d69cdefaa972
Lab NCC <labncc@gmail..com>
2024-06-12 00:00:42 +0700

    update runner script

--- rlscripts/M/ILI/ILI_all_normal.sh
+++ rlscripts/M/ILI/ILI_all_normal.sh
@@ -18,7 +18,7 @@ python -u run_open_net.py \
     --enc_in 7 \
     --dec_in 7 \
     --c_out 7 \
-    --des 'normal' \
+    --des 'normal_0' \
     --n_learner 3 \
     --urt_heads 1 \
     --learning_rate 0.001 \
@@ -41,7 +41,111 @@ python -u run_rlmc.py \
     --pred_len 24 \
     --e_layers 2 \
     --d_layers 1 \
-    --des 'normal' \
+    --des 'normal_0' \
+    --d_model 512 \
+    --seed 42 \
+    --use_weight 0 \
+    --use_td 1 \
+    --use_extra 1 \
+    --use_pretrain 1 \
+    --epsilon 1 \
+    --gamma 0.99 \
+    --tau 0.005 \
+    --exp_name ILI_36_24
+
+python -u run_open_net.py \
+    --is_training 1 \
+    --root_path ./dataset/illness/ \
+    --data_path national_illness.csv \
+    --model_id ILI_36_24 \
+    --model B6iFast \
+    --data custom \
+    --features M \
+    --seq_len 36 \
+    --label_len 18 \
+    --pred_len 24 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --factor 1 \
+    --enc_in 7 \
+    --dec_in 7 \
+    --c_out 7 \
+    --des 'normal_1' \
+    --n_learner 3 \
+    --urt_heads 1 \
+    --learning_rate 0.001 \
+    --d_model 512 \
+    --d_ff 2048 \
+    --itr 1 \
+    --fix_seed 2021 \
+    --train_epochs 20 \
+    --batch_size 32 \
+    --patience 5 \
+    --checkpoints ./checkpoints/
+
+python -u run_rlmc.py \
+    --model_id ILI_36_24 \
+    --model B6iFast \
+    --data custom \
+    --features M \
+    --seq_len 36 \
+    --label_len 18 \
+    --pred_len 24 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --des 'normal_1' \
+    --d_model 512 \
+    --seed 42 \
+    --use_weight 0 \
+    --use_td 1 \
+    --use_extra 1 \
+    --use_pretrain 1 \
+    --epsilon 1 \
+    --gamma 0.99 \
+    --tau 0.005 \
+    --exp_name ILI_36_24
+
+python -u run_open_net.py \
+    --is_training 1 \
+    --root_path ./dataset/illness/ \
+    --data_path national_illness.csv \
+    --model_id ILI_36_24 \
+    --model B6iFast \
+    --data custom \
+    --features M \
+    --seq_len 36 \
+    --label_len 18 \
+    --pred_len 24 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --factor 1 \
+    --enc_in 7 \
+    --dec_in 7 \
+    --c_out 7 \
+    --des 'normal_2' \
+    --n_learner 3 \
+    --urt_heads 1 \
+    --learning_rate 0.001 \
+    --d_model 512 \
+    --d_ff 2048 \
+    --itr 1 \
+    --fix_seed 2021 \
+    --train_epochs 20 \
+    --batch_size 32 \
+    --patience 5 \
+    --checkpoints ./checkpoints/
+
+python -u run_rlmc.py \
+    --model_id ILI_36_24 \
+    --model B6iFast \
+    --data custom \
+    --features M \
+    --seq_len 36 \
+    --label_len 18 \
+    --pred_len 24 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --des 'normal_2' \
     --d_model 512 \
     --seed 42 \
     --use_weight 0 \
@@ -71,7 +175,7 @@ python -u run_open_net.py \
     --enc_in 7 \
     --dec_in 7 \
     --c_out 7 \
-    --des 'normal' \
+    --des 'normal_0' \
     --n_learner 3 \
     --urt_heads 1 \
     --learning_rate 0.001 \
@@ -94,7 +198,111 @@ python -u run_rlmc.py \
     --pred_len 36 \
     --e_layers 2 \
     --d_layers 1 \
-    --des 'normal' \
+    --des 'normal_0' \
+    --d_model 512 \
+    --seed 42 \
+    --use_weight 0 \
+    --use_td 1 \
+    --use_extra 1 \
+    --use_pretrain 1 \
+    --epsilon 1 \
+    --gamma 0.99 \
+    --tau 0.005 \
+    --exp_name ILI_36_36
+
+python -u run_open_net.py \
+    --is_training 1 \
+    --root_path ./dataset/illness/ \
+    --data_path national_illness.csv \
+    --model_id ILI_36_36 \
+    --model B6iFast \
+    --data custom \
+    --features M \
+    --seq_len 36 \
+    --label_len 18 \
+    --pred_len 36 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --factor 1 \
+    --enc_in 7 \
+    --dec_in 7 \
+    --c_out 7 \
+    --des 'normal_1' \
+    --n_learner 3 \
+    --urt_heads 1 \
+    --learning_rate 0.001 \
+    --d_model 512 \
+    --d_ff 2048 \
+    --itr 1 \
+    --fix_seed 2021 \
+    --train_epochs 20 \
+    --batch_size 32 \
+    --patience 5 \
+    --checkpoints ./checkpoints/
+
+python -u run_rlmc.py \
+    --model_id ILI_36_36 \
+    --model B6iFast \
+    --data custom \
+    --features M \
+    --seq_len 36 \
+    --label_len 18 \
+    --pred_len 36 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --des 'normal_1' \
+    --d_model 512 \
+    --seed 42 \
+    --use_weight 0 \
+    --use_td 1 \
+    --use_extra 1 \
+    --use_pretrain 1 \
+    --epsilon 1 \
+    --gamma 0.99 \
+    --tau 0.005 \
+    --exp_name ILI_36_36
+
+python -u run_open_net.py \
+    --is_training 1 \
+    --root_path ./dataset/illness/ \
+    --data_path national_illness.csv \
+    --model_id ILI_36_36 \
+    --model B6iFast \
+    --data custom \
+    --features M \
+    --seq_len 36 \
+    --label_len 18 \
+    --pred_len 36 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --factor 1 \
+    --enc_in 7 \
+    --dec_in 7 \
+    --c_out 7 \
+    --des 'normal_2' \
+    --n_learner 3 \
+    --urt_heads 1 \
+    --learning_rate 0.001 \
+    --d_model 512 \
+    --d_ff 2048 \
+    --itr 1 \
+    --fix_seed 2021 \
+    --train_epochs 20 \
+    --batch_size 32 \
+    --patience 5 \
+    --checkpoints ./checkpoints/
+
+python -u run_rlmc.py \
+    --model_id ILI_36_36 \
+    --model B6iFast \
+    --data custom \
+    --features M \
+    --seq_len 36 \
+    --label_len 18 \
+    --pred_len 36 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --des 'normal_2' \
     --d_model 512 \
     --seed 42 \
     --use_weight 0 \
@@ -124,7 +332,7 @@ python -u run_open_net.py \
     --enc_in 7 \
     --dec_in 7 \
     --c_out 7 \
-    --des 'normal' \
+    --des 'normal_0' \
     --n_learner 3 \
     --urt_heads 1 \
     --learning_rate 0.001 \
@@ -147,7 +355,111 @@ python -u run_rlmc.py \
     --pred_len 48 \
     --e_layers 2 \
     --d_layers 1 \
-    --des 'normal' \
+    --des 'normal_0' \
+    --d_model 512 \
+    --seed 42 \
+    --use_weight 0 \
+    --use_td 1 \
+    --use_extra 1 \
+    --use_pretrain 1 \
+    --epsilon 1 \
+    --gamma 0.99 \
+    --tau 0.005 \
+    --exp_name ILI_36_48
+
+python -u run_open_net.py \
+    --is_training 1 \
+    --root_path ./dataset/illness/ \
+    --data_path national_illness.csv \
+    --model_id ILI_36_48 \
+    --model B6iFast \
+    --data custom \
+    --features M \
+    --seq_len 36 \
+    --label_len 18 \
+    --pred_len 48 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --factor 1 \
+    --enc_in 7 \
+    --dec_in 7 \
+    --c_out 7 \
+    --des 'normal_1' \
+    --n_learner 3 \
+    --urt_heads 1 \
+    --learning_rate 0.001 \
+    --d_model 512 \
+    --d_ff 2048 \
+    --itr 1 \
+    --fix_seed 2021 \
+    --train_epochs 20 \
+    --batch_size 32 \
+    --patience 5 \
+    --checkpoints ./checkpoints/
+
+python -u run_rlmc.py \
+    --model_id ILI_36_48 \
+    --model B6iFast \
+    --data custom \
+    --features M \
+    --seq_len 36 \
+    --label_len 18 \
+    --pred_len 48 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --des 'normal_1' \
+    --d_model 512 \
+    --seed 42 \
+    --use_weight 0 \
+    --use_td 1 \
+    --use_extra 1 \
+    --use_pretrain 1 \
+    --epsilon 1 \
+    --gamma 0.99 \
+    --tau 0.005 \
+    --exp_name ILI_36_48
+
+python -u run_open_net.py \
+    --is_training 1 \
+    --root_path ./dataset/illness/ \
+    --data_path national_illness.csv \
+    --model_id ILI_36_48 \
+    --model B6iFast \
+    --data custom \
+    --features M \
+    --seq_len 36 \
+    --label_len 18 \
+    --pred_len 48 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --factor 1 \
+    --enc_in 7 \
+    --dec_in 7 \
+    --c_out 7 \
+    --des 'normal_2' \
+    --n_learner 3 \
+    --urt_heads 1 \
+    --learning_rate 0.001 \
+    --d_model 512 \
+    --d_ff 2048 \
+    --itr 1 \
+    --fix_seed 2021 \
+    --train_epochs 20 \
+    --batch_size 32 \
+    --patience 5 \
+    --checkpoints ./checkpoints/
+
+python -u run_rlmc.py \
+    --model_id ILI_36_48 \
+    --model B6iFast \
+    --data custom \
+    --features M \
+    --seq_len 36 \
+    --label_len 18 \
+    --pred_len 48 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --des 'normal_2' \
     --d_model 512 \
     --seed 42 \
     --use_weight 0 \
@@ -177,7 +489,7 @@ python -u run_open_net.py \
     --enc_in 7 \
     --dec_in 7 \
     --c_out 7 \
-    --des 'normal' \
+    --des 'normal_0' \
     --n_learner 3 \
     --urt_heads 1 \
     --learning_rate 0.001 \
@@ -200,7 +512,111 @@ python -u run_rlmc.py \
     --pred_len 60 \
     --e_layers 2 \
     --d_layers 1 \
-    --des 'normal' \
+    --des 'normal_0' \
+    --d_model 512 \
+    --seed 42 \
+    --use_weight 0 \
+    --use_td 1 \
+    --use_extra 1 \
+    --use_pretrain 1 \
+    --epsilon 1 \
+    --gamma 0.99 \
+    --tau 0.005 \
+    --exp_name ILI_36_60
+
+python -u run_open_net.py \
+    --is_training 1 \
+    --root_path ./dataset/illness/ \
+    --data_path national_illness.csv \
+    --model_id ILI_36_60 \
+    --model B6iFast \
+    --data custom \
+    --features M \
+    --seq_len 36 \
+    --label_len 18 \
+    --pred_len 60 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --factor 1 \
+    --enc_in 7 \
+    --dec_in 7 \
+    --c_out 7 \
+    --des 'normal_1' \
+    --n_learner 3 \
+    --urt_heads 1 \
+    --learning_rate 0.001 \
+    --d_model 512 \
+    --d_ff 2048 \
+    --itr 1 \
+    --fix_seed 2021 \
+    --train_epochs 20 \
+    --batch_size 32 \
+    --patience 5 \
+    --checkpoints ./checkpoints/
+
+python -u run_rlmc.py \
+    --model_id ILI_36_60 \
+    --model B6iFast \
+    --data custom \
+    --features M \
+    --seq_len 36 \
+    --label_len 18 \
+    --pred_len 60 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --des 'normal_1' \
+    --d_model 512 \
+    --seed 42 \
+    --use_weight 0 \
+    --use_td 1 \
+    --use_extra 1 \
+    --use_pretrain 1 \
+    --epsilon 1 \
+    --gamma 0.99 \
+    --tau 0.005 \
+    --exp_name ILI_36_60
+
+python -u run_open_net.py \
+    --is_training 1 \
+    --root_path ./dataset/illness/ \
+    --data_path national_illness.csv \
+    --model_id ILI_36_60 \
+    --model B6iFast \
+    --data custom \
+    --features M \
+    --seq_len 36 \
+    --label_len 18 \
+    --pred_len 60 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --factor 1 \
+    --enc_in 7 \
+    --dec_in 7 \
+    --c_out 7 \
+    --des 'normal_2' \
+    --n_learner 3 \
+    --urt_heads 1 \
+    --learning_rate 0.001 \
+    --d_model 512 \
+    --d_ff 2048 \
+    --itr 1 \
+    --fix_seed 2021 \
+    --train_epochs 20 \
+    --batch_size 32 \
+    --patience 5 \
+    --checkpoints ./checkpoints/
+
+python -u run_rlmc.py \
+    --model_id ILI_36_60 \
+    --model B6iFast \
+    --data custom \
+    --features M \
+    --seq_len 36 \
+    --label_len 18 \
+    --pred_len 60 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --des 'normal_2' \
     --d_model 512 \
     --seed 42 \
     --use_weight 0 \
