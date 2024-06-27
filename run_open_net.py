f53624281ec2d2f99d9fdd96db4e7ef89f704064
Anggito02 <anggito_anju@yahoo.com>
2024-06-27 15:18:45 +0700

    update run

--- run_open_net.py
+++ run_open_net.py
@@ -153,10 +153,10 @@ def main():
             # opt = OptURT(args)  # set experiments
 
             print('>>>>>>>start training : {}>>>>>>>>>>>>>>>>>>>>>>>>>>'.format(setting))
-            # exp.train(setting)
+            exp.train(setting)
 
             # print('>>>>>>>start training URT: {}>>>>>>>>>>>>>>>>>>>>>>>>>>'.format(setting))
-            # opt.train_urt(setting)
+            opt.train_urt(setting)
 
             # Testing only Mantra
             print('>>>>>>>testing only mantra : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
